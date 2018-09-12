5f81401b71caba90dec255f4720bf63506559f2a
Vaith <vaithwee@yeah.net>
2018-09-12 21:11:38 +0800

    Add first demo

---
+++ VariableDefinition/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariableDefinition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a;
+            int b;
+            double c;
+
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            Console.ReadLine();
+        }
+    }
+}
