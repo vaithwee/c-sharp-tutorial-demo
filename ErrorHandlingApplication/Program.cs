1f0c3ae900dd0dada8c91920694e2a36a44ad688
Vaith <vaithwee@yeah.net>
2018-09-28 17:51:06 +0800

    Add handle error demo

---
+++ ErrorHandlingApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ErrorHandlingApplication
+{   
+    class DivNumbers
+    {
+        int result;
+        DivNumbers()
+        {
+            result = 0;
+        }
+
+        public void Divsion(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught : {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("Result : {0}", result);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DivNumbers d = new DivNumbers();
+            d.Divsion(25, 0);
+            
+        }
+    }
+}
---
+++ UserDefinedException/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UserDefinedException
+{
+    class TestTemperture
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            try
+            {
+                temp.ShowTemp();
+            }
+            catch(TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0}", e.Message);
+            }
+        }
+    }
+
+    public class TempIsZeroException : ApplicationException
+    {
+        public TempIsZeroException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class Temperature
+    {
+        int temperature = 0;
+
+        public void ShowTemp()
+        {
+            if (temperature == 0)
+            {
+                throw (new TempIsZeroException("Zero Temperature found"));
+            }
+            else 
+            {
+                Console.WriteLine("Temprature: {0}", temperature);
+            }
+        }
+    }
+}
