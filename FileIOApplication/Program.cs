a38be2b5f2ef14f00a1a5c664db09950cc25115c
Vaith <vaithwee@yeah.net>
2018-10-05 14:34:14 +0800

    Add simple file io demo

---
+++ FileIOApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream("test.data", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            for (int idx = 0; idx <= 20; idx++)
+            {
+                fileStream.WriteByte((byte)idx);
+            }
+
+            fileStream.Position = 0;
+
+            for (int idx = 0;idx <= 20; ++idx)
+            {
+                Console.WriteLine(fileStream.ReadByte() + " ");
+            }
+
+            fileStream.Close();
+            
+        }
+    }
+}
