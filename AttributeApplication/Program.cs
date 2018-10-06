582e052cc6d4966c072e88c7a24e6c19d5bd416d
Vaith <vaith@VaithdeMacBook-Air.local>
2018-10-06 18:24:49 +0800

    Add some attritute demo

---
+++ AttributeApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿#define DEBUG
+
+using System;
+using System.Diagnostics;
+
+
+namespace AttributeApplication
+{
+    public class Myclass
+    {
+        [Conditional("DEBUG")]
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void function1()
+        {
+            Myclass.Message("In Function 1.");
+            function2();
+        }
+
+        static void function2()
+        {
+            Myclass.Message("In Function 2");
+        }
+        static void Main(string[] args)
+        {
+            Myclass.Message("In Main Function");
+            function1();
+        }
+    }
+}
---
+++ ObsoleteApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObsoleteApplication
+{
+    class Program
+    {
+        [Obsolete("Don't use OldMethod, use NewMethod instead", true)]
+        static void OldMethod()
+        {
+            Console.WriteLine("It is the old method");
+        }
+        static void NewMethod()
+        {
+            Console.WriteLine("It is the new method");
+        }
+        static void Main(string[] args)
+        {
+            OldMethod();
+        }
+    }
+}
