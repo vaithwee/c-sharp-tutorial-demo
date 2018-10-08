e26a69d6cd27bb6d153f89c400bb101fee3e9dee
Vaith <vaithwee@yeah.net>
2018-10-08 11:12:43 +0800

    Add property demo

---
+++ TutorialAppliaction/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace TutorialAppliaction
+{
+    public abstract class Person
+    {
+        public abstract string Name
+        {
+            get;
+            set;
+        }
+
+        public abstract int Age
+        {
+            get;
+            set;
+        }
+    }
+
+    class Student : Person
+    {
+        private string code = "n.a";
+        private string name = "n.a";
+        private int age = 0;
+        public string Code
+        {
+            get
+            {
+                return code;
+            }
+            set
+            {
+                code = value;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public override int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Code = " + code + ", Name = " + name + ", Age = " + age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Student s = new Student();
+           s.Code = "001";
+           s.Name = "Zra";
+           s.Age = 9;
+           Console.WriteLine("Student Info : {0}", s);
+        }
+    }
+}
---
+++ TutorialspointAppliaction/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TutorialspointAppliaction
+{
+    class Student
+    {
+        private string code = "n.a";
+        private string name = "not known";
+        private int age = 0;
+
+        public string Code
+        {
+            set
+            {
+                code = value;
+            }
+            get
+            {
+                return code;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {
+                age = value;
+            }
+            get
+            {
+                return age;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Code = " + code + ", Name = " + name + ", Age = " + age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Student s = new Student();
+           s.Code = "001";
+           s.Name = "Zara";
+           s.Age = 9;
+
+           Console.WriteLine("Student Info : {0}", s);
+           s.Age += 1;
+           Console.WriteLine("Student Info : {0}", s);
+        }
+    }
+}
